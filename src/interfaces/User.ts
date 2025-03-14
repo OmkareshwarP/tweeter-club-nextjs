f182998a4462ea974c1f8edc8885bb51d5bddd74
OmkareshwarP <omece460@gmail.com>
2025-03-14 13:32:25 +0530

    authentication setup done

---
+++ src/interfaces/User.ts
@@ -0,0 +1,10 @@
+interface IAuthUserInfo {
+  userId: string;
+  username: string;
+  name: string;
+  profilePictureMediaId: string;
+  followingCount: number;
+  followersCount: number;
+}
+
+export type { IAuthUserInfo };
